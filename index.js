99bba533a01baffc1d0475f16da1b61a71a99dc2
mauve <ranger@mauve.moe>
2021-06-17 20:55:34 -0400

    Fix passing body and querystring to fetch

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ function createServeHyperdrive (Hyperdrive, {
   const fetch = makeFetch({ Hyperdrive, writable, ...opts })
 
   return async function serveHyperdrive (request, reply) {
-    const { headers, params, method } = request
+    const { headers, params, method, url: requestPath, body } = request
     const { key, '*': path } = params
 
-    const url = `hyper://${key}/${path}`
+    // Lazy way to get the querystring from the URL. :P
+    const { search: queryString } = new URL('http://example' + requestPath)
+
+    const url = `hyper://${key}/${path}${queryString}`
 
     const response = await fetch(url, {
       method,
-      headers
+      headers,
+      body
     })
 
     for (const [header, value] of response.headers) {
